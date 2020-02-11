ba9ac8835fdabbb17eb62624bebe4845935f084a
Niels Julicher <niels.julicher@edu-kw1c.nl>
2020-02-11 12:38:00 +0100

    index.php en opdracht 2.1 toegevoegd

---
+++ hoofdstuk2/opdracht2.1.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: Niels Julicher
+ * Date: 11-2-2020
+ * Time: 12:30
+ * File: opdracht2.1.php
+ */
+?>
+
+<html>
+<head>
+    <title>This is the title</title>
+    <meta name="description" content="...">
+    <meta name="keywords" content="...">
+    <meta charset="UTF-8">
+    <link href="CSS/style.css">
+</head>
+<body>
+    <?php
+        echo("<p>Hello World!</p>");
+    ?>
+    <a href="../index.php">Terug</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * User: Niels Julicher
+ * Date: Date
+ * Time: Time
+ * File: index.php
+ */
+?>
+
+<html>
+<head>
+    <title>This is the title</title>
+    <meta name="description" content="...">
+    <meta name="keywords" content="...">
+    <meta charset="UTF-8">
+    <link href="CSS/style.css">
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+</body>
+</html>
